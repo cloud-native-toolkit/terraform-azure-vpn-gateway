61e76d77ccb3f51bf1bbd0deeffa9851cccb1157
sunmeher <45628173+sunmeher@users.noreply.github.com>
2022-05-25 18:52:37 +0530

    Updated VPN Gateway Variable File

--- main.tf
+++ main.tf
@@ -1,35 +1,25 @@
 
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = "ahm-resources"
-  location = "West Europe"
-}
 
-resource "azurerm_virtual_network" "virtual_network" {
-  name                = "ahm-network"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  address_space       = ["10.0.0.0/16"]
-}
 
 resource "azurerm_virtual_wan" "virtual_wan" {
-  name                = "ahm-vwan"
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location            = azurerm_resource_group.resource_group.location
+  name                = var.virtual_wan_name
+  resource_group_name = var.resource_group_name
+  location            = var.region
 }
 
 resource "azurerm_virtual_hub" "virtual_hub" {
-  name                = "ahm-vhub"
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location            = azurerm_resource_group.resource_group.location
+  name                = var.virtual_hub_name
+  resource_group_name = var.resource_group_name
+  location            = var.region
   virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
-  address_prefix      = "10.0.2.0/24"
+  address_prefix      = var.azurerm_virtual_hub_address_prefix
 }
 
 resource "azurerm_vpn_gateway" "vpn_gateway" {
-  name                = "ahm-vpng"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  name                = var.vpn_gateway_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
   virtual_hub_id      = azurerm_virtual_hub.virtual_hub.id
 }
 
--- variables.tf
+++ variables.tf
@@ -4,3 +4,43 @@
 #  default     = ""
 #}
 
+
+variable "resource_group_name" {
+  type        = string
+  description = "This the name of the Azure resource group where the VPN Gateway has been provisioned"
+}
+
+variable "region" {
+  type        = string
+  description = "This is Azure location where the VPN Gateway will be created"
+}
+
+variable "virtual_network_name" {
+  type        = string
+  description = "This the Azure virtual network name where the VPN Gateway will be provisioned"
+}
+
+variable "virtual_wan_name" {
+  type        = string
+  description = "This is Azure virtual WAN name where the VPN Gateway will be provisioned"
+}
+
+variable "virtual_hub_name" {
+  type        = string
+  description = "This is the Virtual hub name where Azure VPN Gateway will be provisoned"
+}
+
+variable "vpn_gateway_name" {
+  type        = string
+  description = "This is the VPN Gateway name to be used in the VPN Gateway"
+}
+
+variable "azurerm_virtual_hub_address_prefix" {
+  type        = string
+  description = "This value will define the address prefix ( IP Address set ) on Azure Virtual Virtual Hub"
+}
+
+variable "virtual_network_address_space" {
+  type = string
+  description = "This valie will define the address space IP Range"
+}
