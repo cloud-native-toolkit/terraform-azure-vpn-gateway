eb3a29ffde24f0327667b8df56dc7270e06e12d2
sunmeher <45628173+sunmeher@users.noreply.github.com>
2022-06-02 15:16:23 +0530

    updated main.tf with VPN Gateway resources
    Signed-off-by: sunmeher <45628173+sunmeher@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,22 +1,75 @@
 
+#======================================================================================
+# Manages a (VPN Gateway) Virtual Network Gateway to establish a secure, 
+# cross-premises connectivity.
+#======================================================================================
 
-resource "azurerm_virtual_wan" "virtual_wan" {
-  name                = var.virtual_wan_name
-  resource_group_name = var.resource_group_name
-  location            = var.region
+#===============================
+# Resource Group, VNet, Subnet 
+#===============================
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.region
 }
 
-resource "azurerm_virtual_hub" "virtual_hub" {
-  name                = var.virtual_hub_name
-  resource_group_name = var.resource_group_name
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = var.virtual_network_name
   location            = var.region
-  virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
-  address_prefix      = var.virtual_hub_address_prefix
+  resource_group_name = var.resource_group_name
+  address_space       = var.virtual_network_address_space
 }
 
-resource "azurerm_vpn_gateway" "vpn_gateway" {
+resource "azurerm_subnet" "subnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = var.subnet_address_space
+}
+
+#=================================================
+# Public IP, Virtual Network Gateway, Certificates 
+#=================================================
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  allocation_method   = var.public_ip_allocation_method
+}
+
+resource "azurerm_virtual_network_gateway" "virtual_network_gateway" {
   name                = var.vpn_gateway_name
   location            = var.region
   resource_group_name = var.resource_group_name
-  virtual_hub_id      = azurerm_virtual_hub.virtual_hub.id
+
+  type     = var.gateway_type
+  vpn_type = var.vpn_type
+
+  active_active = var.enable_active_active
+  enable_bgp    = var.enable_bgp
+  sku           = var.vpn_gw_sku
+
+  ip_configuration {
+    name                          = var.vpn_ip_configuration
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    private_ip_address_allocation = var.private_ip_address_allocation_method
+    subnet_id                     = azurerm_subnet.subnet.id
+  }
+
+  vpn_client_configuration {
+    address_space = var.vpn_client_config_address_space
+
+    root_certificate {
+      name             = var.root_certificate_name
+      public_cert_data = var.public_cert_data_details
+    }
+
+    revoked_certificate {
+      name       = var.revoked_certificate_details
+      thumbprint = var.thumbprint_revoke
+
+    }
+  }
 }
+
--- variables.tf
+++ variables.tf
@@ -5,42 +5,114 @@
 #}
 
 
+
 variable "resource_group_name" {
   type        = string
-  description = "This the name of the Azure resource group where the VPN Gateway has been provisioned"
+  description = "Name of the Azure resource group where the VPN Gateway has been provisioned."
 }
 
 variable "region" {
   type        = string
-  description = "This is Azure location where the VPN Gateway will be created"
+  description = "Location/region to keep all your network resources"
 }
 
 variable "virtual_network_name" {
   type        = string
-  description = "This the Azure virtual network name where the VPN Gateway will be provisioned"
+  description = "Name of the virtual network"
 }
 
-variable "virtual_wan_name" {
-  type        = string
-  description = "This is Azure virtual WAN name where the VPN Gateway will be provisioned"
+variable "virtual_network_address_space" {
+  type        = list(string)
+  description = "This will define the required Address Space and CIDR value"
 }
 
-variable "virtual_hub_name" {
+variable "subnet_address_space" {
+  type        = list(string)
+  description = "This will define the required Subnet Address Space and CIDR value"
+}
+
+variable "public_ip_name" {
   type        = string
-  description = "This is the Virtual hub name where Azure VPN Gateway will be provisoned"
+  description = "It provide the Public IP for the VPN Gateway"
+}
+
+variable "public_ip_allocation_method" {
+  type        = string
+  description = "Defines the allocation method for this IP address. Possible values are Static or Dynamic. Defaults is Dynamic"
+  default     = "Dynamic"
 }
 
 variable "vpn_gateway_name" {
   type        = string
-  description = "This is the VPN Gateway name to be used in the VPN Gateway"
+  description = "Name of the Virtual Network Gateway"
 }
 
-variable "virtual_hub_address_prefix" {
+variable "gateway_type" {
   type        = string
-  description = "This value will define the address prefix ( IP Address set ) on Azure Virtual Virtual Hub"
+  description = "The type of the Virtual Network Gateway. Options are Vpn or ExpressRoute"
+  default     = "Vpn"
 }
 
-variable "virtual_network_address_space" {
-  type = string
-  description = "This valie will define the address space IP Range"
+variable "vpn_type" {
+  type        = string
+  description = "The routing type of the Virtual Network Gateway. Options are RouteBased or PolicyBased. Defaults is RouteBased"
+  default     = "RouteBased"
 }
+
+variable "enable_active_active" {
+  type        = bool
+  description = "An active-active Virtual Network Gateway will be created, if - true. And it requires a HighPerformance or an UltraPerformance sku. An active-standby gateway will be created, if - false. Defaults to false."
+  default     = false
+}
+
+variable "enable_bgp" {
+  type        = bool
+  description = "BGP (Border Gateway Protocol) will be enabled if - True for this Virtual Network Gateway. Defaults to false"
+  default     = false
+}
+
+variable "vpn_gw_sku" {
+  type        = string
+  description = "Size and capacity of the virtual network gateway. Options are Basic, VpnGw1, VpnGw2"
+  default     = "Basic"
+}
+
+variable "vpn_ip_configuration" {
+  type        = string
+  description = "Virtual network gateway IP Configuration name details"
+  default     = "vnetGatewayConfig"
+}
+
+variable "private_ip_address_allocation_method" {
+  type        = string
+  description = "Defines the allocation method for this IP address. Values are Static or Dynamic. Defaults is Dynamic"
+  default     = "Dynamic"
+}
+
+variable "vpn_client_config_address_space" {
+  type        = list(string)
+  description = "The address space out of which IP addresses for vpn clients will be taken. You can provide the address space in CIDR notation."
+}
+
+variable "root_certificate_name" {
+  type        = string
+  description = "The root certificates is used to sign the client certificate used by the VPN clients to connect to the gateway"
+}
+
+variable "public_cert_data_details" {
+  type        = string
+  description = "The public certificate of the root certificate authority. The certificate must be provided in Base-64 encoded X.509 format (PEM). In particular, this argument must not include the -----BEGIN CERTIFICATE----- or -----END CERTIFICATE----- markers."
+}
+
+variable "revoked_certificate_details" {
+  type        = string
+  description = "A user-defined name of the revoked certificate."
+  default     = ""
+}
+
+variable "thumbprint_revoke" {
+  type        = string
+  description = "The SHA1 thumbprint of the certificate to be revoked"
+  default     = ""
+}
+
