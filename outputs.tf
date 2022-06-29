d45afad3b99dfd7c915bfa2ea14764cc9e495b04
sunmeher <45628173+sunmeher@users.noreply.github.com>
2022-06-29 12:33:46 +0530

    updated output.tf file
    Signed-off-by: sunmeher <45628173+sunmeher@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
 output "vpn_gateway_name" {
   description = "Name of the Azure VPN Gateway created"
-  value       = azurerm_virtual_network_gateway.vpn_gateway_name.name
-}
+  value       = azurerm_virtual_network_gateway.virtual_network_gateway.name
+  }
 
 output "vpn_gateway_public_ip" {
   description = "The Public IP of the Virtual Network Gateway."
-  value       = azurerm_public_ip.public_ip.ip_address.id
+  value       = azurerm_public_ip.public_ip.id
 }
