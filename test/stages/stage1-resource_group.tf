80e0b6765fda4369ae243c4df0063c3b35fc6c28
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-08-24 15:09:07 +1000

    Added random naming for resource group testing
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage1-resource_group.tf
+++ test/stages/stage1-resource_group.tf
@@ -1,6 +1,12 @@
+resource "random_string" "cluster_id" {
+    length = 5
+    special = false
+    upper = false
+}
+
 module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-azure-resource-group"
 
-  resource_group_name = "${var.name_prefix}-rg"
+  resource_group_name = "${var.name_prefix}-${random_string.cluster_id.result}-rg"
   region              = var.region
 }
