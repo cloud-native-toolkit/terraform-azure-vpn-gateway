df7f0c13a1923d6be638b7c8601cea4699479644
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-08-24 14:11:22 +1000

    Cleaned up test stages
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage1-resource_group.tf
+++ test/stages/stage1-resource_group.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-azure-resource-group"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.name_prefix}-rg"
   region              = var.region
 }
